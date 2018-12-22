2d8eb07fdaeacf9a90e6d26b1269c70432ffba63
storres8 <stevetorres7562@gmail.com>
2018-12-22 16:13:29 -0500

    fixed the one to one relationship, and fixed 2 typos regarding the models.

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -2,11 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfileInfo(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(
+        User,
+        on_delete = models.CASCADE,
+        primary_key = True,
+    )
 
     # additional classes
-    portfolio_site= model.URLField(blank=True)
-    profile_pic = model.ImageField(upload_to='profile_pics', blank=True)
+    portfolio_site= models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
 
     def __str__(self):
         return self.user.username
