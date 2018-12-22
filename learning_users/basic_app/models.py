bea0d21fa4744d47e4f42906c3e005b3ca8c18b7
storres8 <stevetorres7562@gmail.com>
2018-12-22 15:39:58 -0500

    set up users model.

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfileInfo(models.Model):
+    user = models.OneToOneField(User)
+
+    # additional classes
+    portfolio_site= model.URLField(blank=True)
+    profile_pic = model.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
